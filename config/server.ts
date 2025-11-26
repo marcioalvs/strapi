4f4f1062a0e9921ad42607cb2ff24e9008aac7db
marcioalvs <marcioalvs@gmail.com>
2025-11-26 17:39:12 -0300

    .

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("BACKEND_PUBLIC_URL", "http://localhost:1337"), // Ensure this is set
   app: {
     keys: env.array('APP_KEYS'),
   },
